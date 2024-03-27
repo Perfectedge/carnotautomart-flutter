0a8d951ce2f40bbc378d4db0464dea02eb637d25
Kingkar Dev <kingkar.dev01@gmail.com>
2024-03-27 16:25:36 +0600

    review page design completed

--- lib/ui/post details/review_screen.dart
+++ lib/ui/post details/review_screen.dart
@@ -18,94 +18,114 @@ class _ReviewScreenState extends State<ReviewScreen> {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Scaffold(
-      appBar: CarnotMartAppbar(
-        title: 'Review',
-      ),
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SpaceHelper.verticalSpace(15.0),
-              Text(
-                "Review Data",
-                style: textTheme.bodySmall?.copyWith(color: Colors.black),
-              ),
-              SpaceHelper.verticalSpace(15.0),
-              Text(
-                "1 Comments",
-                style: textTheme.bodySmall?.copyWith(color: Colors.black),
-              ),
-              SpaceHelper.verticalSpace(10.0),
-              ListView.separated(
-                padding: EdgeInsets.zero,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 2,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return Row(
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(35.0),
-                          child: CachedNetworkImage(
-                            imageUrl:
-                                "https://st4.depositphotos.com/1000423/23971/i/450/depositphotos_239719906-stock-photo-networking-as-global-concept.jpg",
-                            height: 70,
-                            width: 70,
-                            fit: BoxFit.cover,
-                          )),
-                      SpaceHelper.horizontalSpaceSmall,
-                      Expanded(
-                        child: SizedBox(
-                          height: 65,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: Text(
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      "Peter Omorugie",
-                                      style: textTheme.bodySmall?.copyWith(color: Colors.black,fontWeight: FontWeight.w400),
-                                    ),
-                                  ),
-                                  Text(
-                                    textAlign: TextAlign.center,
-                                    "15 Nov,2020 06:06 AM ",
-                                    style: textTheme.bodySmall?.copyWith(color: Colors.black54, fontSize: 10.0,fontWeight: FontWeight.w400),
-                                  )
-                                ],
-                              ),
-                              SpaceHelper.verticalSpace(10.0),
-                              Text(
-                                  maxLines: 3,
-                                  overflow: TextOverflow.ellipsis,
-                                  "hi..still available?",
-                                  style: textTheme.bodySmall?.copyWith(color: Colors.black54, fontSize: 12.0))
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return SpaceHelper.verticalSpace(10.0);
-                },
-              ),
-              CommentBox(
-                width: 200,
-                title: "jkjkj",
-              )
-            ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: const CarnotMartAppbar(
+          title: 'Review',
+        ),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SpaceHelper.verticalSpace(15.0),
+                Text(
+                  "Review Data",
+                  style: textTheme.bodySmall?.copyWith(color: Colors.black),
+                ),
+                SpaceHelper.verticalSpace(15.0),
+                Text(
+                  "2 Comments",
+                  style: textTheme.bodySmall?.copyWith(color: Colors.black),
+                ),
+                SpaceHelper.verticalSpace(10.0),
+                ListView.separated(
+                  padding: EdgeInsets.zero,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 2,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return DynamicCommentShow();
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return SpaceHelper.verticalSpace(10.0);
+                  },
+                ),
+                SpaceHelper.verticalSpace(30.0),
+                const CommentBox(
+                  title: "Type your message",
+                )
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class DynamicCommentShow extends StatelessWidget {
+  const DynamicCommentShow({
+    super.key,
+    
+  });
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    final  textTheme =Theme.of(context).textTheme;;
+    return Row(
+      children: [
+        ClipRRect(
+            borderRadius: BorderRadius.circular(35.0),
+            child: CachedNetworkImage(
+              imageUrl:
+                  "https://st4.depositphotos.com/1000423/23971/i/450/depositphotos_239719906-stock-photo-networking-as-global-concept.jpg",
+              height: 70,
+              width: 70,
+              fit: BoxFit.cover,
+            )),
+        SpaceHelper.horizontalSpaceSmall,
+        Expanded(
+          child: SizedBox(
+            height: 65,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        "Peter Omorugie",
+                        style: textTheme.bodySmall?.copyWith(color: Colors.black, fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                    Text(
+                      textAlign: TextAlign.center,
+                      "15 Nov,2020 06:06 AM ",
+                      style: textTheme.bodySmall?.copyWith(color: Colors.black54, fontSize: 10.0, fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
+                SpaceHelper.verticalSpace(10.0),
+                Text(
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    "hi..still available?",
+                    style: textTheme.bodySmall?.copyWith(color: Colors.black54, fontSize: 12.0))
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/ui/widget/comment_box.dart
+++ lib/ui/widget/comment_box.dart
@@ -1,59 +1,52 @@
 import 'package:carnotautomart/ui/utils/app_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CommentBox extends StatelessWidget {
-   CommentBox({super.key,this.width,required this.title});
+ const CommentBox({super.key, this.width, required this.title});
 
-  double? width;
-  String title;
+final  double? width;
+ final String title;
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-      height: 40.0,
-      child: Material(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                color: Colors.white,
-                elevation: 0,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                            width: width ??double.infinity,
-                            height: 40,
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Center(
-                              child: TextFormField(
-                                // controller: _chatController.messageController,
-                                autofocus: false,
-                                // maxLines: null,
-                                minLines: 1,
-                                decoration: InputDecoration(
-                                  isDense: true,
-                                  contentPadding: EdgeInsets.zero,
-                                  border: InputBorder.none,
-                                  //  border:_border ,
-                                  // enabledBorder: _border,
-                                  //  focusedBorder: _border,
-                                  hintText: title,
-                                  hintStyle: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal, color: colorDarkAsh),
-                                ),
-                                
-                                style: const TextStyle(fontSize: 16.0, color: Colors.black),
-                                onChanged: (text) {},
-                                onTapOutside: (value) {
-                                  // log('onTapOutside called');
-                                },
-                              ),
-                            ),
-                      ),
-                    ),
-                    Icon(CupertinoIcons.arrow_up_right_circle)
-                  ],
+    return Material(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      color: Colors.white,
+      elevation: 0,
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              width: width ?? double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Center(
+                child: TextFormField(
+                  autofocus: false,
+                  maxLines: 30,
+                  minLines: 1,
+                  decoration: InputDecoration(
+                    isDense: false,
+                    contentPadding: EdgeInsets.zero,
+                    border: InputBorder.none,
+                    hintText: title,
+                    hintStyle: const TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal, color: colorDarkAsh),
+                  ),
+                  style: const TextStyle(fontSize: 14.0, color: Colors.black),
+                  onChanged: (text) {},
+                  onTapOutside: (value) {
+                    // log('onTapOutside called');
+                  },
                 ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: Icon(CupertinoIcons.arrow_up_right_circle),
+          )
+        ],
       ),
     );
   }
